75f741f012cd7a3905c51d5f320e6e0ec31931c6
Jitendra Sengar <jitendrasengar1517@gmail.com>
2024-04-11 07:54:26 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -11,7 +11,14 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
 
+//routes import
+import router from "./routes/user.routes.js"
+import { registerUser } from "./controllers/user.controller.js";
 
+router.route("/register").post(registerUser)
+
+//routes declaration
+app.use("/api/v1/users",router)
 
 
 export default (app);
