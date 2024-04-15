646acf2398a3716f80eb9daef72b836a21ad83e9
Jitendra Sengar <jitendrasengar1517@gmail.com>
2024-04-15 07:11:01 +0530

    add login and logoutuser

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constrants.js";
+// import { DB_NAME } from "../constrants.js";
 
 const connectDB = async () => {
   try {
-       const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}`)
       
         console.log(`MongoDB Connected!!! DB HOST:${connectionInstance.connection.host}`);
       
