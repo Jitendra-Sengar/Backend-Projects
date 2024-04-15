3dff4a94a7bfa4e4c95f231a95adae8cfdd8eac4
Jitendra Sengar <jitendrasengar1517@gmail.com>
2024-04-15 07:52:42 +0530

    add login logoutuser

--- src/middlewares/auth.midlleware.js
+++ src/middlewares/auth.midlleware.js
@@ -1,23 +1,25 @@
-import { User } from "../models/user.models";
-import { ApiError } from "../utils/ApiError";
-import { asyncHandler } from "../utils/asyncHandler";
-import { User } from "../models/user.models";
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-export const verifyJWT=asyncHandler(async(req,res,next)=>{
-    try {
-        const token=req.cookies?.accesstoekn || req.header("Authorization")?.replace("Bearer ","")
-        if(!token){
-            throw new ApiError(401,"Unauthorized access")
-        }
-        const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
-        const user=await User.findById(decodedToken?._id).select("-password -refreshToken")
-        if(!user){
-            throw new ApiError(401,"Invalid access Token")
-        }
-        req.user=user;
-        next()
-    } catch (error) {
-        throw new ApiError(401,error?.message || "Invalid Access Token")
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accesstoekn ||
+      req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      throw new ApiError(401, "Unauthorized access");
     }
-    
-})
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+    if (!user) {
+      throw new ApiError(401, "Invalid access Token");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access Token");
+  }
+});
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,34 +1,26 @@
-import {v2 as cloudinary} from 'cloudinary';
-import { log } from 'console';
-import fs from "fs"
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+import fs from "fs";
 
-          
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary=async (localFilePath)=>{
-    try {
-        if(!localFilePath) return null
-        //upload the file on cloudinary
-        const response=await cloudinary.uploader.upload(localFilePath,{
-            resource_type:"auto"
-        })
-        //file is uploaded on cloudinary
-        console.log("File is uploaded on cloudinary",response.url);
-        return response
-    } catch (error) {
-        fs.unlinkSync(localFilePath) //remove the locally saveed file if the opration is failed
-    }
-}
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //file is uploaded on cloudinary
+    console.log("File is uploaded on cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove the locally saveed file if the opration is failed
+  }
+};
 
-
-
-cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
-
-
-  export {uploadOnCloudinary}
+export { uploadOnCloudinary };
